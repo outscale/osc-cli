603db504666e642594be20a51fb9ab9d5f3edeec
Jerome Jutteau <jerome.jutteau@outscale.com>
2019-01-10 13:59:45 +0100

    Rename configuration file
    Renamed config.conf to config.json to avoid repetition
    and provide a feedback on config format.
    
    Signed-off-by: Jerome Jutteau <jerome.jutteau@outscale.com>

--- osc_sdk/sdk.py
+++ osc_sdk/sdk.py
@@ -13,7 +13,7 @@ import xmltodict
 SDK_VERSION = '0.1'
 USER_AGENT = 'osc_sdk ' + SDK_VERSION
 CONFIGURATION_FOLDER = '.osc_sdk'
-CONFIGURATION_FILE = 'config.conf'
+CONFIGURATION_FILE = 'config.json'
 
 SSL_VERIFY = True
 DEFAULT_METHOD = 'POST'
