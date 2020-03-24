a14a936bc93cb59558e86c70edcebc40f560bb50
francois.rabanel <46570624+outscale-frl@users.noreply.github.com>
2020-03-24 18:04:36 +0100

    [OSSCall] remove if parameters into get_parameters func (#36)

--- osc_sdk/sdk.py
+++ osc_sdk/sdk.py
@@ -469,9 +469,7 @@ class OSCCall(JsonApiCall):
             parameters.setdefault(head_key, {})
             self.format_data(parameters[head_key], queue_key, value)
         else:
-            parameters[key] = (
-                value[1:-1].split(',') if value.startswith('[') else value
-            )
+            parameters[key] = value
 
 
     def build_url(self, call):
