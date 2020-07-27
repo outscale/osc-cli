d4e7746026418b161f3a228d0fad320f993de51c
Harsh Raj Singhal <harsh-raj.singhal@outscale.com>
2020-07-27 13:53:58 +0200

    Version updated: 1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='osc-sdk',
-    version='1.4',
+    version='1.5',
     packages=find_packages(),
     author='Outscale SAS',
     author_email='contact@outscale.com',
