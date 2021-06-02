2d78efd23afb35aa0f8b7b7d9f714e6d26888f5e
Jérôme Jutteau <jerome.jutteau@outscale.com>
2021-06-02 15:27:13 +0200

    tests: smartcard tests
    Add some tests for:
    - softhsm (v2)
    - yubikey (disabled by default)
    
    Test configuration must be setup in tests/config.env
    
    closes #66
    
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- tests/test_int.sh
+++ tests/test_int.sh
@@ -22,4 +22,22 @@ for t in *.sh ; do
     ./$t
 done
 
+if $OSC_TEST_SMARTCARD_SOFTHSM; then
+    $PROJECT_ROOT/tests/clean_config_files.sh
+    $PROJECT_ROOT/tests/gen_smartcard_softhsm_config_file.sh
+    cd $PROJECT_ROOT/tests/generic_tests/
+    for t in *.sh ; do
+	./$t
+    done
+fi
+
+if $OSC_TEST_SMARTCARD_YUBIKEY; then
+    $PROJECT_ROOT/tests/clean_config_files.sh
+    $PROJECT_ROOT/tests/gen_smartcard_yubikey_config_file.sh
+    cd $PROJECT_ROOT/tests/generic_tests/
+    for t in *.sh ; do
+	./$t
+    done
+fi
+
 $PROJECT_ROOT/tests/clean_config_files.sh
