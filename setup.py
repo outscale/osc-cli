b85f33f994ca1f7df0fd9c797d39d7697da3336b
Jérôme Jutteau <jerome.jutteau@outscale.com>
2022-11-07 14:58:36 +0100

    osc-cli v1.9.0
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="osc-sdk",
-    version="1.8.0",
+    version="1.9.0",
     packages=find_packages(),
     author="Outscale SAS",
     author_email="contact@outscale.com",
