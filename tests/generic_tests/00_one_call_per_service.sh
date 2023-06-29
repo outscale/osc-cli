c1df0802451ed0ce55e6b071bfd0f8cb8e573c5c
Matthias Gatto <matthias.gatto@outscale.com>
2023-06-29 17:44:32 +0200

    add local_test.sh
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- tests/generic_tests/00_one_call_per_service.sh
+++ tests/generic_tests/00_one_call_per_service.sh
@@ -14,20 +14,21 @@ setup_osc_config_file_accesskey
 # Test simple calls on each service (preferably with mandatory arguments)
 name="osc-cli-test-${RANDOM}${RANDOM}${RANDOM}"
 
-$c api CreateKeypair --KeypairName $name &> /dev/null || { echo "API error (keypair creation)"; exit 1; }
-$c api DeleteKeypair --KeypairName $name &> /dev/null || { echo "API error (keypair deletion)"; exit 1; }
+$c api $ENDPOINT_CLI_ARG CreateKeypair --KeypairName $name &> /dev/null || { echo "API error (keypair creation)"; exit 1; }
+$c api $ENDPOINT_CLI_ARG DeleteKeypair --KeypairName $name &> /dev/null || { echo "API error (keypair deletion)"; exit 1; }
 
-$c fcu CreateKeyPair --KeyName $name &> /dev/null || { echo "FCU error (keypair creation)"; exit 1; }
-$c fcu DeleteKeyPair --KeyName $name &> /dev/null || { echo "FCU error (keypair deletion)"; exit 1; }
 
-$c lbu CreateLoadBalancer --LoadBalancerName $name --Listeners '[{"Protocol":"HTTP", "LoadBalancerPort":"80", "InstanceProtocol":"HTTP", "InstancePort":"80"}]' --AvailabilityZones '["eu-west-2a"]' &> /dev/null || { echo "LBU error (load balancer creation)"; exit 1; }
-$c lbu DeleteLoadBalancer --LoadBalancerName $name &> /dev/null || { echo "LBU error (load balancer deletion)"; exit 1; }
+$c fcu $ENDPOINT_CLI_ARG CreateKeyPair --KeyName $name &> /dev/null || { echo "FCU error (keypair creation)"; exit 1; }
+$c fcu $ENDPOINT_CLI_ARG DeleteKeyPair --KeyName $name &> /dev/null || { echo "FCU error (keypair deletion)"; exit 1; }
 
-try_hard $c eim CreateUser --UserName $name &> /dev/null || { echo "EIM error (user creation)"; exit 1; }
-try_hard $c eim DeleteUser --UserName $name &> /dev/null || { echo "EIM error (user deletion)"; exit 1; }
+$c lbu $ENDPOINT_CLI_ARG CreateLoadBalancer --LoadBalancerName $name --Listeners '[{"Protocol":"HTTP", "LoadBalancerPort":"80", "InstanceProtocol":"HTTP", "InstancePort":"80"}]' --AvailabilityZones '["eu-west-2a"]' &> /dev/null || { echo "LBU error (load balancer creation)"; exit 1; }
+$c lbu $ENDPOINT_CLI_ARG DeleteLoadBalancer --LoadBalancerName $name &> /dev/null || { echo "LBU error (load balancer deletion)"; exit 1; }
 
-try_hard $c icu ReadConsumptionAccount --FromDate 2021-06-01 --ToDate 2021-06-02 &> /dev/null || { echo "ICU error (consumption reading)"; exit 1; }
+try_hard $c eim $ENDPOINT_CLI_ARG CreateUser --UserName $name &> /dev/null || { echo "EIM error (user creation)"; exit 1; }
+try_hard $c eim $ENDPOINT_CLI_ARG DeleteUser --UserName $name &> /dev/null || { echo "EIM error (user deletion)"; exit 1; }
 
-$c directlink DescribeConnections &> /dev/null || { echo "DirectLink error (connection reading)"; exit 1; }
+try_hard $c icu $ENDPOINT_CLI_ARG ReadConsumptionAccount --FromDate 2021-06-01 --ToDate 2021-06-02 &> /dev/null || { echo "ICU error (consumption reading)"; exit 1; }
+
+$c directlink $ENDPOINT_CLI_ARG DescribeConnections &> /dev/null || { echo "DirectLink error (connection reading)"; exit 1; }
 
 echo "OK"
--- tests/generic_tests/01_auth_access_key.sh
+++ tests/generic_tests/01_auth_access_key.sh
@@ -12,11 +12,11 @@ echo -n "$(basename $0): "
 setup_osc_config_file_accesskey
 
 # Test one simple call on each service with explicit access key
-$c api ReadNets --authentication-method=accesskey &> /dev/null || { echo "API error"; exit 1; }
-$c fcu DescribeVpcs --authentication-method=accesskey &> /dev/null || { echo "FCU error"; exit 1; }
-$c lbu DescribeLoadBalancers --authentication-method=accesskey &> /dev/null || { echo "LBU error"; exit 1; }
-try_hard $c eim ListServerCertificates --authentication-method=accesskey &> /dev/null || { echo "EIM error"; exit 1; }
-try_hard $c icu ReadQuotas --authentication-method=accesskey &> /dev/null || { echo "ICU error"; exit 1; }
-$c directlink DescribeConnections --authentication-method=accesskey &> /dev/null || { echo "DirectLink error"; exit 1; }
+$c api  $ENDPOINT_CLI_ARG ReadNets --authentication-method=accesskey &> /dev/null || { echo "API error"; exit 1; }
+$c fcu  $ENDPOINT_CLI_ARG DescribeVpcs --authentication-method=accesskey &> /dev/null || { echo "FCU error"; exit 1; }
+$c lbu  $ENDPOINT_CLI_ARG DescribeLoadBalancers --authentication-method=accesskey &> /dev/null || { echo "LBU error"; exit 1; }
+try_hard $c eim $ENDPOINT_CLI_ARG ListServerCertificates --authentication-method=accesskey &> /dev/null || { echo "EIM error"; exit 1; }
+try_hard $c icu $ENDPOINT_CLI_ARG ReadQuotas --authentication-method=accesskey  &> /dev/null || { echo "ICU error"; exit 1; }
+$c directlink $ENDPOINT_CLI_ARG DescribeConnections --authentication-method=accesskey &> /dev/null || { echo "DirectLink error"; exit 1; }
 
 echo "OK"
